59002b7c0be05d1b782a73c61199b34ec4348ad8
Steven Imus <imussg@gmail.com>
2018-05-15 14:37:15 -0400

    store has correct data

--- scripts/index.js
+++ scripts/index.js
@@ -18,7 +18,7 @@ const store = {
 
 // TASK: Add the Youtube Search API Base URL here:
 // Documentation is here: https://developers.google.com/youtube/v3/docs/search/list#usage
-const BASE_URL = 'https://www.googleapis.com/youtube/v3/search?';
+const BASE_URL = 'https://www.googleapis.com/youtube/v3/search';
 
 // TASK:
 // 1. Create a `fetchVideos` function that receives a `searchTerm` and `callback`
@@ -26,11 +26,16 @@ const BASE_URL = 'https://www.googleapis.com/youtube/v3/search?';
 // 3. Make a getJSON call using the query object and sending the provided callback in as the last argument
 // TEST IT! Execute this function and console log the results inside the callback.
 const fetchVideos = function(searchTerm, callback) {
-  const specificUrl = `${BASE_URL}key=${API_KEY}&part=snippet&q=${searchTerm}`;
-  const jsonReturn = $.getJSON(specificUrl, callback);
-  console.log(jsonReturn);
+  const query = {
+    part: 'snippet',
+    key: API_KEY,
+    q: searchTerm
+  };
+  // const specificUrl = `${BASE_URL}key=${API_KEY}&part=snippet&q=${searchTerm}`;
+  $.getJSON(BASE_URL, query, response => callback(response));
+  // console.log(jsonReturn.responseJSON);
   // console.log(jsonReturn["responseJSON"].items);
-  return jsonReturn.responseJSON;
+  // return jsonReturn.responseJSON;
 };
 
 // TASK:
@@ -42,7 +47,14 @@ const fetchVideos = function(searchTerm, callback) {
 // TEST IT! Grab an example API response and send it into the function - make sure
 // you get back the object you want.
 const decorateResponse = function(response) {
-
+  const modified = response.items.map(item => {
+    return {
+      id: item.id.videoId,
+      title: item.snippet.title,
+      thumbnail: item.snippet.thumbnails.default.url
+    };
+  });
+  addVideosToStore(modified);
 };
 
 // TASK:
@@ -50,7 +62,12 @@ const decorateResponse = function(response) {
 // 2. Using the object, return an HTML string containing all the expected data
 // TEST IT!
 const generateVideoItemHtml = function(video) {
-
+  return `
+  <li>
+    <h3>${video.title}</h3>
+    <img src="${video.thumbnail}" />
+  </li>
+  `;
 };
 
 // TASK:
@@ -58,7 +75,10 @@ const generateVideoItemHtml = function(video) {
 // objects and sets the array as the value held in store.items
 // TEST IT!
 const addVideosToStore = function(videos) {
-
+  for(let i=0; i<videos.length; i++) {
+    store.videos.push(videos[i]);
+  }
+  console.log(store);
 };
 
 // TASK:
@@ -86,7 +106,7 @@ const handleFormSubmit = function() {
     event.preventDefault();
     const searchTerm = $('input#search-term').val();
     $('input#search-term').val('');
-    const query = fetchVideos(searchTerm);
+    const response = fetchVideos(searchTerm, decorateResponse);
   });
 };
 
